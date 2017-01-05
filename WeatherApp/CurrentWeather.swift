c640133708092bb0eca7b6c7036dd74db18a09ed
Prateek Mahawar <PrateekMahawar@gmail.com>
2017-01-05 21:33:05 +0530

    DictionaryComplete

--- WeatherApp/CurrentWeather.swift
+++ WeatherApp/CurrentWeather.swift
@@ -55,17 +55,36 @@ class CurrentWeather {
         
         Alamofire.request(currentWeatherURL).responseJSON { response in
             
-            if let JSON = response.result.value {
-                print("JSON: \(JSON)")
+        let JSON = response.result.value
+            print(JSON)
+            if let dict = response.result.value as? Dictionary<String,AnyObject> {
+                
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    print(self._cityName)
+                }
+                
+                if let weather = dict["weather"] as? [Dictionary<String,AnyObject>] {
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        print(self._weatherType)
+                    }
+                }
+                
+                if let main = dict["main"] as? Dictionary<String,AnyObject> {
+                    
+                    if let currentTemperature = main["temp"] as? Double {
+                        let kelvinToCelsius = currentTemperature - 273.15
+                        let RoundCelsius = String(format: "%.2f", kelvinToCelsius)
+                        self._currentTemp = Double(RoundCelsius)
+                        print(self._currentTemp)
+                    }
+                }
+                
+                }
             }
+            completed()
             
         }
-//        Alamofire.request(.GET, currentWeatherURL).responseJSON { response in
-//            let result = response.result
-//            print(result)
-//        }
-        completed()
         
-    }
-    
 }
